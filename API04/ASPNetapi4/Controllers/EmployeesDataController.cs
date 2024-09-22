e59bc3258edaea939e61535755a65be1c6db505c
vishalkumarsingh015 <vishalpratapsingh143047@gmail.com>
2024-09-22 16:15:29 +0530

    MVCRESTAPI04
    MVCRESTAPI04

---
+++ API04/ASPNetapi4/Controllers/EmployeesDataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNetapi4.Controllers
+{
+    public class EmployeesDataController : ApiController 
+    {
+    
+        public string[] myEmployee = { "vishal", "adi", "pk" };
+
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return myEmployee;
+        }
+        [HttpGet]
+        public string  GetEmployeeByIndex(int id)
+        {
+            return myEmployee[id];
+        }
+    }
+}
